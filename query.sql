4e3f8ba5dcb55692830eebaaaa9d6da01fd962fc
Erika Yi <68488125+akire2iy@users.noreply.github.com>
2020-10-23 16:36:30 -0400

    fifth commit

---
+++ query.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM health_cities;
+
+SELECT *
+FROM hospital_data_2010;
+
+SELECT *
+FROM hospital_data_2011;
+
+SELECT *
+FROM hospital_data_2012;
+
+SELECT *
+FROM hospital_data_2013;
+
+SELECT *
+FROM hospital_data_2014;
+
+SELECT *
+FROM hospital_data_2015;
